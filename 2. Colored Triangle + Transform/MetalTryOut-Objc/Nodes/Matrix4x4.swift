8970828b67e81d6b2f8658775caa94999eb4d4a2
haawa799 <haawaplus@gmail.com>
2014-06-25 16:19:50 +0300

    Fed structures added

---
+++ 2. Colored Triangle + Transform/MetalTryOut-Objc/Nodes/Matrix4x4.swift
@@ -0,0 +1,13 @@
+//
+//  Matrix4x4.swift
+//  Triangle
+//
+//  Created by Andrew K. on 6/25/14.
+//  Copyright (c) 2014 Andrew Kharchyshyn. All rights reserved.
+//
+
+import UIKit
+
+class Matrix4x4: NSObject {
+   
+}
---
+++ 2. Colored Triangle + Transform/MetalTryOut-Objc/Shaders/VertexStruct.swift
@@ -0,0 +1,13 @@
+//
+//  VertexStruct.swift
+//  Triangle
+//
+//  Created by Andrew K. on 6/25/14.
+//  Copyright (c) 2014 Andrew Kharchyshyn. All rights reserved.
+//
+
+import UIKit
+
+class VertexStruct: NSObject {
+   
+}
